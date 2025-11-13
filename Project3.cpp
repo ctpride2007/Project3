4475442f962651fc7d050f521b5aeda0a388056d
ctpride07 <ctpride07@gmail.com>
2025-11-12 21:36:41 -0500

    Add project files.

---
+++ Project3.cpp
@@ -0,0 +1,24 @@
+/*
+*Carter Pride
+* Professor Schaffner
+* Write a program that produces a bar chart for the population growth of a town. The data is stored in intervals over the past several years, with the population for that year (rounded to the nearest 1,000 people) in each line of the file. A test data file called People.txt has been provided on Canvas that contains the population numbers for several years. For this data set, the starting year is 1900 and the interval between data points is 20. 
+The program should ask for the name of the data file and the name of the town. The program should open the file and verify that it has opened with no errors. Then the program should read the population figure, calculate the year, and display the year and population on a bar chart. For each year, it should display the year and a bar consisting of one asterisk for each 1,000 people.
+Here is an example of how the chart might begin:
+My Town Population Growth
+(each * represents 1,000 people)
+1900 **
+1920 ****
+1940 *****
+* 11/2025
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
